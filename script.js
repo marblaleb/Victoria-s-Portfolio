9eb537e0f3550979f35f82b3f4eec42fff048f77
marblaleb <marcoblaleb@gmail.com>
2024-08-11 16:23:07 +0200

    next

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 const body = document.querySelector('body');
 const intro = document.querySelector('.intro'),
@@ -15,6 +15,8 @@ const init = () => {
     initLenis();
     initScrollHero();
     initScrollMedia();
+    initConnection();
+    initHorizontal();
 }
 
 const initLenis = () => {
@@ -115,6 +117,77 @@ const initGalleryText = () => {
 
 
 
+
+
+const initConnection= () =>{
+    const connect= document.querySelector('.connect'),
+        connectMedia=document.querySelectorAll('.connect_media');
+
+    connectMedia.forEach((media) => {
+
+        media.classList.contains('image--front')
+        ? gsap.set(media, {xPercent: -200, y: 0, yPercent: -50})
+        : gsap.set(media, {xPercent: 200, y: 0, yPercent: -50});
+    });
+
+    const tlConnect= gsap.timeline({
+        ease: 'none',
+        scrollTrigger: {
+            trigger: connect,
+            start: 'top top',
+            end: '+=3000',
+            scrub: 1,
+            pin: true
+        }
+    });
+
+    tlConnect.to(connectMedia, {
+        xPercent: 0,
+
+    }).to(connectMedia, {scale: 0.5})
+
+}
+
+
+
+
+
+
+const initHorizontal= () =>{
+
+    const horizontal= document.querySelector('.horizontal'),
+    horizontalVerticalBoxes=horizontal.querySelectorAll('.horizontal_box--vertical');
+
+    const tlHorizontal=gsap.timeline({
+        defaults: {ease: 'none'},
+        scrollTrigger: {
+            trigger: horizontal,
+            start: 'top top',
+            end: ()=> '+=' + horizontal.offsetWidth,
+            pin: true,
+            scrub: 1,
+            invalidateOnRefresh: true,
+        },
+    });
+
+    tlHorizontal.to('.horizontal_wrapper', {
+        x: ()=> -(horizontal.scrollWidth - document.documentElement.clientWidth) + 'px',
+    });
+
+    gsap.set(horizontalVerticalBoxes, {y: '-25%'});
+    tlHorizontal.to(
+        horizontalVerticalBoxes,
+        {
+            y: '25%',
+            stagger: 0.02,
+        },
+        0
+    );
+
+};
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
     if (!isMobile.matches) {
@@ -124,6 +197,7 @@ window.addEventListener('DOMContentLoaded', () => {
     else {
         initLenis();
         initScrollMedia();
+       
     }
 
 
